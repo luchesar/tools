12867d2354a0108b52e0cf829cc48114bcf82945
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-10-29 22:21:57 +0000

    A working version which can migrate entries

--- mongo2git.go
+++ mongo2git.go
@@ -2,57 +2,112 @@ package main
 
 import (
   "fmt"
+  "errors"
   "gopkg.in/mgo.v2"
   "io/ioutil"
   "os"
+  "log"
+  "github.com/libgit2/git2go"
   "encoding/json"
-//  "gopkg.in/mgo.v2/bson"
+  "time"
+  "runtime"
+  "path"
+)
+
+const (
+  MasterRef = "refs/heads/master"
 )
 
 var mongoConnection = "mongodb://54.171.48.210/coinvision"
 var mongoDb = "coinvision"
 var mongoCollection = "bitfinex_l2"
-var gitRepoPath = os.Args[1]
 
+var gitRepoPath = os.Args[1]
+  
 func main() {
-  sess, err := mgo.Dial(mongoConnection)
-  if err != nil {
-    fmt.Printf("Can't connect to mongo %v\n", err)
-    os.Exit(1)
-  }
-  defer sess.Close() 
+  sess, err := mgo.Dial(mongoConnection); try(err)
+  defer sess.Close()
+
+  gitRepo, err := git.OpenRepository(gitRepoPath); try(err)
+  defer gitRepo.Free()
 
   sess.SetSafe(&mgo.Safe{})
   collection := sess.DB(mongoDb).C(mongoCollection)
 
   var result map[string]interface{}
   iter := collection.Find(nil).Iter()
+  defer try(iter.Close())
   for i := 0; i < 5 ; i++ {
     iter.Next(&result)
-    err = updateFile(result)
-    if err != nil {
-      fmt.Println("Cannot update a mongo entry %v", err )
-    }
+    updateFile(gitRepo, result)
   }
-  if err := iter.Close(); err != nil {
+}
+
+func updateFile(gitRepo *git.Repository, book map[string]interface{}) {
+  defer catch("Cannot handle a record")
+  b, err := json.MarshalIndent(book, "", " "); try(err)
+  var filePath = path.Clean(path.Dir(gitRepoPath) + "/" + mongoCollection)
+  try(ioutil.WriteFile(filePath, b, 0644))
+  fmt.Printf("file %s written with size %d and id %v\n", filePath,len(b), book["_id"])
+  
+  gitAddAndCommit(gitRepo, []string{mongoCollection}, "book id " + fmt.Sprintf("%v", book["_id"]))
+}
+
+func gitAddAndCommit(repo *git.Repository, paths []string, message string) {
+    if message == "" {
+        panic(errors.New("commit message empty"))
+    }
+
+    tree := gitAdd(repo, paths...)
+    defer tree.Free()
+
+    gitCommit(repo, message, tree)
+}
+
+func gitCommit(repo *git.Repository, message string, tree *git.Tree) {
+    signature := &git.Signature{
+        Name:  "lucho",
+        Email: "luchesar.cekov@gmail.com",
+        When:  time.Now(),
+    }
+    parentCommit := make([]*git.Commit, 0)
+
+    master, err := repo.LookupReference(MasterRef); try(err)
+    defer master.Free()
+
+    objId := master.Target()
+    c, err := repo.LookupCommit(objId); try(err)
+
+    parentCommit = append(parentCommit, c)
+    _, err = repo.CreateCommit(MasterRef, signature, signature, message, tree, parentCommit...); try(err)
+}
+
+
+func gitAdd(repo *git.Repository, paths ...string) *git.Tree {
+    indx, err := repo.Index(); try(err)
+    defer indx.Free()
+
+    // Go through all the filepaths and add them to the indx
+    for _, file := range paths {
+        try(indx.AddByPath(file))
+    }
+
+    try(indx.Write())
+    treeID, err := indx.WriteTree(); try(err)
+    tree, err := repo.LookupTree(treeID); try(err)
+    return tree
+}
+
+func try(err error) {
+  if (err != nil) {
     panic(err)
   }
 }
 
-func updateFile(book map[string]interface{}) error {
-  b, err := json.MarshalIndent(book, "", " ")
-  if err != nil {
-    return err
-  }
-  err = ioutil.WriteFile(gitRepoPath + "/" + mongoCollection, b, 0644)
-  if (err != nil) {
-    return err
-  }
-  fmt.Printf("book: %s\n\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", b)
-  return nil
-}
-
-func commit() error {
-  return nil
+func catch(message string) {
+    if r := recover(); r != nil {
+        _, fn, line, _ := runtime.Caller(1)
+        log.Printf("[error] %s:%d %v", fn, line, r)
+    }
 }
 
